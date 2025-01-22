f2f474657446413d1b0dafbd9bb6928b34040cea
Gerard87564 <ggonzalezpujolas@gmail.com>
2025-01-22 20:40:43 +0100

    fed

---
+++ env/make_admin.py
@@ -0,0 +1,9 @@
+from app import app, db, User 
+with app.app_context():
+    user = User.query.filter_by(username='shout').first()
+    if user:
+        user.is_admin = True
+        db.session.commit()
+        print(f"El usuario '{user.username}' ahora es administrador.")
+    else:
+        print("Usuario no encontrado.")
