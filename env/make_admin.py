fae80e522310a0416c5a0d8b74e5ce78f907aca4
Gerard87564 <ggonzalezpujolas@gmail.com>
2025-01-27 14:47:06 +0100

    rf

--- env/make_admin.py
+++ env/make_admin.py
@@ -1,6 +1,6 @@
 from app import app, db, User 
 with app.app_context():
-    user = User.query.filter_by(username='ggonzalezpujolas').first()
+    user = User.query.filter_by(username='josep').first()
     if user:
         user.is_admin = True
         db.session.commit()
