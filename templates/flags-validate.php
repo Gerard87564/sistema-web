f17a70b5deb2f37c6ac34ae4eb9df2d9a5cc13d3
Gerard87564 <ggonzalezpujolas@gmail.com>
2025-02-22 12:51:46 +0100

    fed

---
+++ templates/flags-validate.php
@@ -0,0 +1,33 @@
+<?php
+    $ftp_server = "192.168.1.46"; 
+    $ftp_user_name = "scruz";  
+    $ftp_user_pass = "1234";  
+    $ftp_directory = "/flags"; 
+
+    $conn_id = ftp_connect($ftp_server);
+    if (!$conn_id) {
+        die("No se pudo conectar al servidor FTP $ftp_server");
+    }
+
+    $login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
+    if (!$login_result) {
+        die("Conexión FTP fallida");
+    }
+
+    if (isset($_GET['flag'])) {
+        $flag = $_GET['flag']; 
+
+        ftp_chdir($conn_id, $ftp_directory);
+        $file_list = ftp_nlist($conn_id, ".");
+
+        if (in_array($flag, $file_list)) {
+            echo "<p style='color: green;'>¡Flag correcta!</p>";
+        } else {
+            echo "<p style='color: red;'>Flag incorrecta. Inténtalo de nuevo.</p>";
+        }
+    } else {
+        echo "<p style='color: red;'>No se recibió ninguna flag.</p>";
+    }
+
+    ftp_close($conn_id);
+?>
