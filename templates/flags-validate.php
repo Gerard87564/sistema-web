76e7c56b20456b4510ea65ad953faf39842e9535
Gerard87564 <ggonzalezpujolas@gmail.com>
2025-02-24 18:55:55 +0100

    ef

--- templates/flags-validate.php
+++ templates/flags-validate.php
@@ -1,9 +1,9 @@
 <?php
-    $ftp_server = "192.168.1.46"; 
+    $ftp_server = "10.0.5.210"; 
     $ftp_user_name = "scruz";  
     $ftp_user_pass = "1234";  
     $ftp_directory = "/srv/ftp"; 
-    
+
     $conn_id = ftp_connect($ftp_server);
     if (!$conn_id) {
         die("No se pudo conectar al servidor FTP $ftp_server");
