762c5c362b7655d2ed391421a7fedf10521c26ed
Gerard87564 <ggonzalezpujolas@gmail.com>
2025-02-26 12:34:48 +0100

    fe

--- templates/connexioDB.php
+++ templates/connexioDB.php
@@ -1,7 +1,7 @@
 <?php
     $db_host = "10.0.5.210"; 
-    $db_user = "dvwa";     
-    $db_pass = "p@ssw0rd"; 
+    $db_user = "root";     
+    $db_pass = ""; 
     $db_name = "dvwa";       
     $db_port = 3306;
 
