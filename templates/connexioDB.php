4fe421b1474c2343990ce94531ee86557af700ef
Gerard87564 <ggonzalezpujolas@gmail.com>
2025-02-26 20:06:55 +0100

    wd

--- templates/connexioDB.php
+++ templates/connexioDB.php
@@ -1,11 +1,12 @@
 <?php
     $db_host = "10.0.3.15"; 
-    $db_user = "root";     
-    $db_pass = " ";
+    $db_user = "gerard";     
+    $db_pass = "educem123*A";
     $db_name = "dvwa";       
     $db_port = 3306;
+    $db_socket = "/var/run/mysqld/mysqld.sock"; 
 
-    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
+    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port, $db_socket);
 
     if ($conn->connect_error) {
         die("Conexión fallida: " . $conn->connect_error);
